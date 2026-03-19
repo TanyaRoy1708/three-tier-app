080fff119d89fe9e28134b0c239431e28e9713b5
TanyaRoy1708 <roytanya2800@gmail.com>
2026-03-19 19:23:52 +0530

    updated changes

--- Jenkins-Server-TF/iam-policy.tf
+++ Jenkins-Server-TF/iam-policy.tf
@@ -1,5 +1,14 @@
-resource "aws_iam_role_policy_attachment" "iam-policy" {
-  role = aws_iam_role.iam-role.name
-  # Just for testing purpose, don't try to give administrator access
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role_policy_attachment" "ecr-policy" {
+  role       = aws_iam_role.iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "eks-policy" {
+  role       = aws_iam_role.iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "vpc-policy" {
+  role       = aws_iam_role.iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess" # Needed for ELB and Security Group management via EKS
 }
